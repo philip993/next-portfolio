62b3f854bc2392bdb8f976e0ba361d3d0589c55d
Filip FICO <fitz993m@gmail.com>
2020-08-05 00:10:47 +0200

    1.6. Redux

---
+++ redux/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import { NavbarReducer } from '../components/Navbar/NavbarReducer';
+const rootReducer = combineReducers({
+  NavbarReducer,
+});
+
+export default rootReducer;
