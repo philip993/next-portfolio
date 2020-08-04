86efbeb938bad4856ffa0a4fe8091eae6a84becc
Filip FICO <fitz993m@gmail.com>
2020-08-05 00:07:39 +0200

    1.1. Added Pages

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout/Layout';
+
+const Contact = () => (
+  <Layout>
+    <div>
+      <h1>Contact</h1>
+      <ul>
+        <li>f.mihajlovski93@gmail.com</li>
+        <li>LinkedIn</li>
+      </ul>
+    </div>
+  </Layout>
+);
+
+export default Contact;
