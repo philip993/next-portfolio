3af75a441d4e657b7e9fc790b59837fa021ad4fa
Filip FICO <fitz993m@gmail.com>
2020-08-06 00:48:29 +0200

    2.2. Code clean-up

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,43 +1,32 @@
 import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import { setActiveTrue, setActiveFalse } from './NavbarActions';
 
 const Navbar = () => {
-  const { isActive } = useSelector((state) => state.NavbarReducer);
-  const dispach = useDispatch();
-
-  const linkActive = () => {
-    dispach(setActiveTrue());
-  };
-
-  const linkInactive = () => {
-    dispach(setActiveFalse());
-  };
-
   return (
     <div>
       <ul>
         <li>
-          <Link
-            href="/"
-            onChange={isActive === true ? linkActive : linkInactive}
-          >
+          <Link href="/">
             <a>Home</a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a onChange={isActive ? linkActive : linkInactive}> About</a>
+            <a> About</a>
           </Link>
         </li>
         <li>
           <Link href="/projects">
-            <a onChange={isActive ? linkActive : linkInactive}>Projects</a>
+            <a>Projects</a>
           </Link>
         </li>
         <li>
           <Link href="/contact">
-            <a onChange={isActive ? linkActive : linkInactive}>Contact</a>
+            <a>Contact</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/cv">
+            <a>CV</a>
           </Link>
         </li>
       </ul>
